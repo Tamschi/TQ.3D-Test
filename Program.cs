e3af60f5d96ec2464ae8024f78dd7f17d4b8406c
Tamschi <MailTamschi@googlemail.com>
2019-02-12 19:10:52 +0100

    Enabled alpha blending

--- Program.cs
+++ Program.cs
@@ -248,6 +248,8 @@ namespace TQ._3D_Test
             Gl.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
             Gl.Enable(EnableCap.CullFace);
+            Gl.Enable(EnableCap.Blend);
+            Gl.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
             Gl.CullFace(CullFaceMode.Front);
             Gl.FrontFace(FrontFaceDirection.Ccw);
             _program.Use();
@@ -255,8 +257,9 @@ namespace TQ._3D_Test
             _texture.BindUnit(0);
             foreach (var (first, count) in _drawRanges)
             { Gl.DrawElements(PrimitiveType.Triangles, count * 3, DrawElementsType.UnsignedShort, (IntPtr)(first * sizeof(ushort))); }
-
+            Gl.Disable(EnableCap.Blend);
             Gl.Disable(EnableCap.CullFace);
+
             _boneProgram.Use();
             _boneVao.Bind();
             Gl.DrawElements(PrimitiveType.Lines, _boneLinkCount * 2, DrawElementsType.UnsignedShort, IntPtr.Zero);
